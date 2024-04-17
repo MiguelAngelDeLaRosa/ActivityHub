fbdc959d633f3acfb5bb1f51312cb0df5055e230
Fernando <josefer.hdeza@hotmail.com>
2024-04-16 20:20:37 -0700

    ADD: express server, database Mongo conecction

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import conectarDB from "./config/db.js";
+import dotenv from 'dotenv';
+
+const app = express();
+
+app.use(express.json());
+
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Express conectado desde el puerto: ${PORT}`);
+})
